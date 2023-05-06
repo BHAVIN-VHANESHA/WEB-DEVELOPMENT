d0b2302e60020c89e3f86f5e09acab9072083591
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-06 23:06:19 +0530

    NaN

---
+++ JS/NaN.js
@@ -0,0 +1,16 @@
+//NaN is a property of the global object.
+
+
+//isNaN() is used to check whether it is number or not
+var number = 576845478;
+console.log(isNaN(number))
+var name = "bhavin";
+console.log(isNaN(name));
+
+
+console.log(NaN === NaN);
+console.log(Number.NaN === NaN);
+console.log(isNaN(NaN));
+console.log(isNaN(Number.NaN));
+console.log(isNaN(Number.isNaN(NaN)));
+console.log(Number.isNaN(NaN));
