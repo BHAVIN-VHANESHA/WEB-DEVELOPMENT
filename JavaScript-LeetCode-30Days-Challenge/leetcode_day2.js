4fff62eafc38f03178535fb04a4019616925eb08
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-06 21:47:14 +0530

    js

---
+++ JavaScript-LeetCode-30Days-Challenge/leetcode_day2.js
@@ -0,0 +1,15 @@
+//Given an integer n, return a counter function. 
+//This counter function initially returns n and then 
+//returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+
+
+//solution
+function makeCounter(n) {
+  let count = n;
+  return function() {
+    const result = count;
+    count += 1;
+    return result;
+  };
+} 
