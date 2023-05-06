a25e701f4bdeadcd7ac926dffb7f76d292222eb4
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-06 22:48:15 +0530

    null vs undefined

---
+++ JS/datatypes.js
@@ -0,0 +1,18 @@
+// let n = null;
+// let n;
+
+//undefined
+var m;
+console.log(m);
+console.log(typeof(m));//output is undefined
+
+//null
+var m = null;
+console.log(m);
+console.log(typeof(m));//output is object
+// but null is not an object so this is exception or bug
+
+//null becoz declared after assigning null
+var m;
+console.log(m);
+console.log(typeof(m));//output is object
