541a67d6e4965e3fc2d937b8eb3d5841e04fdf18
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-07 20:14:37 +0530

    functions

---
+++ JS/functions.js
@@ -0,0 +1,30 @@
+// function add(a,b) {
+//     console.log(a+b);    
+// }
+// add(10,5);
+// var exp = add(15,5);
+// exp;
+
+
+//return
+// function add(a,b) {
+//     return a+b;    
+// }
+// add(10,5);
+// var exp = add(15,5);
+// console.log(exp);/
+
+
+
+
+
+//anonymous function
+// function add(a,b) {
+//     return function(){
+
+//     };    
+//}
+var add = function(a,b){
+    return a+b;
+}
+console.log(add(5,5));
