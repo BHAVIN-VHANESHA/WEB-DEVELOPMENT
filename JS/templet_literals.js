3ae9862c09916b7021efc8135559f02d034102b0
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-09 11:13:41 +0530

    literal

---
+++ JS/templet_literals.js
@@ -0,0 +1,5 @@
+for (let index = 1; index <= 10; index++) {
+    const table = 6;
+    // console.log(tabble + " * " + index + " = " + table*index);
+    console.log(`${table} x ${index} = ${table*index}`);
+}
