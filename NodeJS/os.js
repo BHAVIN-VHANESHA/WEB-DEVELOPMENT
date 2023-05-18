341afec0e4777c6cb319f6568e2086f517b25e8c
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-18 12:26:35 +0530

    os

---
+++ NodeJS/os.js
@@ -0,0 +1,8 @@
+const os = require('os');
+// console.log(os.freemem());//free memeory
+// console.log(os.homedir());//home directory
+//  console.log(os.hostname());//host name
+//   console.log(os.platform());//platform
+//   console.log(os.release());//rerlease version
+//    console.log(os.totalmem());//total memory
+//    console.log(os.uptime());//system started from
