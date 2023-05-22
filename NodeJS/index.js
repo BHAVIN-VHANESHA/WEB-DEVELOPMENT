9cb108690ef6d45efbb0f5e6823b59a40ac49d41
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-22 15:33:53 +0530

    index

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,31 +1,30 @@
-//this is the syntax in node js  
-// const http = require('http'); const hostname = '127.0.0.1'; const port = 3000; const server = http.createServer((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end('Hello World'); }); server.listen(port, hostname, () => { 
-    // console.log(`Server running at http://${hostname}:${port}/`); });
+//this is the syntax in node js
+// const http = require('http'); const hostname = '127.0.0.1'; const port = 3000; const server = http.createServer((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end('Hello World'); }); server.listen(port, hostname, () => {
+// console.log(`Server running at http://${hostname}:${port}/`); });
 
+//this is the syntax of express framework of node js
+const express = require("express");
+const app = express();
+const port = 3000;
 
-//this is the syntax of express framework of node js 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req,res) => {
-    res.send('hello world')
-})
-app.get('/p', (req,res) => {
-    res.send('hi i am bahvin')
-})
-app.get('/about', (req,res) => {
-    res.send('i am a programmer')
-})
-app.get('/home', (req,res) => {
-    res.send('i live in navsari')
-})
-app.get('/edu', (req,res) => {
-    res.send('i study at BVM in VVN, Anand')
-})
-app.get('/ambition', (req,res) => {
-    res.send('i want to become a trillonear business man')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.get("/p", (req, res) => {
+  res.send("hi i am bahvin");
+});
+app.get("/about", (req, res) => {
+  res.json({ programmer: 22, develpoer: 22 });
+});
+app.get("/home", (req, res) => {
+  res.send("i live in navsari");
+});
+app.get("/edu", (req, res) => {
+  res.send("i study at BVM in VVN, Anand");
+});
+app.get("/ambition", (req, res) => {
+  res.send("i want to become a trillonear business man");
+});
 app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
-})
+  console.log(`listening at http://localhost:${port}`);
+});
