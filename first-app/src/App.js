95d6666bff7d0875b88a3ec6b4e620356a63990b
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-26 22:51:25 +0530

    first-app

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Form from "./Components/Form";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -9,9 +10,15 @@ function App() {
       <Navbar title="props" about="prop-about" home="prop-home"/> 
       {/* props are used to reuse the same code */}
       {/* <Navbar/> //defaultPropTypes */}
-      <div className="container my-4">
+      
+      {/* <div className="container my-4">
       <Form heading="Text Manipulation" />
+      </div> */}
+
+      <div className="container my-3">
+        <About/>
       </div>
+        
     </>
   );
 }
