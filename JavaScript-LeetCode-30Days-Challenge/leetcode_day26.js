eb93710a2925d666ee24c6df1333df247f36647e
BHAVIN VHANESHA <bhavinyvhanesha@gmail.com>
2023-05-30 08:01:53 +0530

    day 26

---
+++ JavaScript-LeetCode-30Days-Challenge/leetcode_day26.js
@@ -0,0 +1,41 @@
+// Enhance all functions to have the callPolyfill method. The method accepts an object obj as it's first parameter and any number of additional arguments. The obj becomes the this context for the function. The additional arguments are passed to the function (that the callPolyfill method belongs on).
+
+// For example if you had the function:
+
+// function tax(price, taxRate) {
+//   const totalCost = price * (1 + taxRate);
+//   console.log(`The cost of ${this.item} is ${totalCost}`);
+// }
+// Calling this function like tax(10, 0.1) will log "The cost of undefined is 11". This is because the this context was not defined.
+
+// However, calling the function like tax.callPolyfill({item: "salad"}, 10, 0.1) will log "The cost of salad is 11". The this context was appropriately set, and the function logged an appropriate output.
+
+// Please solve this without using the built-in Function.call method.
+
+
+
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+    const fn = this;
+  const uniqueProp = Symbol(); // Create a unique property key
+
+  // Add the function as a property on the object
+  context[uniqueProp] = fn;
+
+  // Execute the function with the object as the this context
+  const result = context[uniqueProp](...args);
+
+  // Remove the temporary property from the object
+  delete context[uniqueProp];
+
+  return result;
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
